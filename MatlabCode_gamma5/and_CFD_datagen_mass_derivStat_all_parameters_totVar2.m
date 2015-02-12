33e4abc07d1f3ddd38c4447d20c9ca347934d2c9
masomatics <koyama.masanori@gmail.com>
2015-02-12 23:44:54 +0900

    Add no denominator integral version (did not work) and Add experimental record in nopic.m

---
+++ MatlabCode_gamma5/and_CFD_datagen_mass_derivStat_all_parameters_totVar2.m
@@ -0,0 +1,128 @@
+
+%
+% THIS VERSTION GOT RID OF THE INTEGRAL IN THE DENOMINATOR!!!!!!
+%
+% 
+
+%
+% This is the diffusion version of Dave's CFD paper. 
+%
+% dXt = A Xt dt + dW.  
+% 
+% rnsource  2 by  sample number by numtimepoints 
+% dataset   2 by  sample number by numtimepoints 
+% init      2 by 1
+%
+% snapshots snapshots.  Each column corresponds to a time.
+% snaptime  sampled time. length is num_frames. 
+%
+
+function [derivative, energy] = and_CFD_datagen_mass_derivStat_all_parameters_totVar2...
+                        (init, tend, theta, sigV, sigW, num_timepts, rnsource, snapshots, snaptime, N)
+                    
+            include= [];
+            
+%%%preset variables %%%                     
+    [species, num_particles, num_slices] = size(snapshots);  
+    h = waitbar(0,'Adabra Catabra'); 
+    delta = tend / num_timepts;
+    num_parameters = 4;    
+    rxn_mat =   [1 0 -1 0; 0 1 0 -1]; 
+    %This part is specific for "ALL particles case"
+    compress_snap_wgts = 1/num_particles * ones(num_particles, num_slices);    
+    
+%%%Target variables%%%     
+    derivative = zeros(num_parameters,1);
+    tilde_p_ymk = zeros(num_particles);
+    p_ymkj = zeros(num_particles, N);
+    dp_jr = zeros(N,num_particles);
+    dEP_kr= zeros(N, num_particles); 
+%%%Temporary variables%%%    
+    data_now = NaN(species, N); 
+    rxn_rate = zeros(4,N);
+    ftheta = NaN(num_parameters, N); 
+    ycopy = zeros(species, num_particles, N); 
+    xcopy = zeros(species, num_particles, N);
+    scale_p_ymkj = zeros(1,N) ;
+    scale_p_ymkj_mat = zeros(num_particles,N);
+    derivative_alltime = zeros(num_parameters, num_slices); 
+ 
+%%%Initialization%%%
+    v = sigV*rnsource;
+    initx = repmat(init,1,N);
+    data_now = initx;
+    timemat = delta*(0:(num_timepts+1));    
+    deriv_loglike = zeros(num_parameters,N);
+    snaptime_now  = 1;
+
+    
+%%%Debug : Assessment variables %%%             
+    energy = zeros(1, num_slices);  
+    p_ymk_all = zeros(num_particles, num_slices); 
+
+%%%Monte Carlo Simulation%%%     
+    for(m = 2 : (num_timepts+1))
+        
+
+        rxn_rate(1,:) = theta(1); 
+        rxn_rate(2,:) = theta(2) * data_now(1,:);
+        rxn_rate(3,:) = theta(3) * data_now(1,:);
+        rxn_rate(4,:) = theta(4) * data_now(2,:);        
+        
+        waitbar(m/num_timepts);
+        %%% ftheta part is considered here.
+        deriv_loglike(1,:) = deriv_loglike(1,:) +  rxn_mat(:,1)'*(v(:,:,m-1)*sqrt(delta))/(sigV^2);
+        deriv_loglike(2,:) = deriv_loglike(2,:) +  data_now(1,:).*(rxn_mat(:,2)'*(v(:,:,m-1)*sqrt(delta)))/(sigV^2)   ;  
+        deriv_loglike(3,:) = deriv_loglike(3,:) +  data_now(1,:).*(rxn_mat(:,3)'*(v(:,:,m-1)*sqrt(delta)))/(sigV^2)  ;
+        deriv_loglike(4,:) = deriv_loglike(4,:) +  data_now(2,:).*(rxn_mat(:,4)'*(v(:,:,m-1)*sqrt(delta)))/(sigV^2)  ;        
+        
+        %Update the data
+        data_now = data_now + rxn_mat*rxn_rate*delta + sqrt(delta)*v(:,:,m-1);
+
+        
+        if(timemat(m) == snaptime(snaptime_now))
+
+            %%% New Codes %%% 
+            ycopy = repmat(snapshots(:,:,snaptime_now), [1,1,N]);
+            xcopy = permute(repmat(data_now,[1,1,num_particles])...
+                ,[1,3,2]);
+             distance_pair = min(squeeze(sum((xcopy - ycopy).^2,1)/sigW^2), 300);
+            p_ymkj = exp(-(distance_pair));
+%INTEGRAL DENOMINATOR            scale_p_ymkj = 1./sum(p_ymkj,1);
+%            scale_p_ymkj_mat = diag(scale_p_ymkj); %Multiplying diag will
+%            cost O(N^3). Elementwise will cost O(N^2).  
+%INTEGRAL DENOMINATOR        scale_p_ymkj_mat = repmat(scale_p_ymkj,[num_particles,1]) ;
+    %tilde_pym in the old version
+%INTEGRAL DENOMINATOR         p_ymkj = p_ymkj .* scale_p_ymkj_mat;
+            tilde_p_ymk = mean(p_ymkj,2);
+            dEP_kr = 1/N *p_ymkj * deriv_loglike' ;
+            
+            derivative_alltime(:,snaptime_now) = sign(tilde_p_ymk - ...
+                compress_snap_wgts(:,snaptime_now))' * (dEP_kr);    
+            energy(snaptime_now) = sum(abs(tilde_p_ymk -compress_snap_wgts(:,snaptime_now)));
+            snaptime_now = snaptime_now  +1;
+
+                   if(max(p_ymkj(:))  > min(p_ymkj(:)) )
+                        %In the case of using empirical distribution,
+                        %the true distribution can be soo far from the
+                        %simulated distribution that the energy can be
+                        %wrongfully too high (all uniform) This part is
+                        %very hand-wavy.... I am ridding of such frames
+                        %from the consideration.                         
+                        include = [include, snaptime_now];
+                   end
+
+        end %end of frame
+                               
+
+    end %end of time loop 
+    derivative = mean(derivative_alltime(:,(include-1)),2);
+    close(h);
+end 
+
+function [rxn_rate] = compute_rxn_rate(theta, data)
+        rxn_rate(1,:) = theta(1); 
+        rxn_rate(2,:) = theta(2) * data(1,:);
+        rxn_rate(3,:) = theta(3) * data(1,:);
+        rxn_rate(4,:) = theta(4) * data(2,:);
+end 
--- MatlabCode_gamma5/and_CFD_snapshot_analyis_nopic_totVar2.m
+++ MatlabCode_gamma5/and_CFD_snapshot_analyis_nopic_totVar2.m
@@ -26,7 +26,7 @@ snapshots = datapts(:, :, timeindex) + rnsource2;
 
 %save('snapshots_allparameters.mat', 'snapshots')
 
-energyhistory_cum + []
+
 %%
 hoge = load('snapshots_allparameters.mat');
 snapshots = hoge.snapshots;
@@ -39,23 +39,15 @@ rnsource = randn([2, Ntry, num_timepts]);
 
 eta = 0.05; 
 %num_iter = 1600;
-num_iter = 500;
+num_iter = 2000;
 
 %theta0 = [0.11054      7.1887];
 %theta0 = [0.16072      8.2049]
 %theta0 = [1.9474 9  0.2331 0.8698]; 
-%timesample = [ 1     2     3     4     5];
-%theta0= rand(1,4).^2;
-theta0 = [1.9578      8.3635     0.25871     0.74678]; %1600th
-theta0 = [1.9075      8.6019     0.39438     0.92227]; %1750th
-theta0 = [1.9664      8.9375     0.23683     0.84142]; %3250th
-theta0 = [1.9648      8.9795     0.23459     0.84587]; %3550th
-theta0 = [1.9704      9.0167     0.24467     0.84785]; %3900th
-theta0 = [1.9628       9.032     0.23819     0.85425]; %4100th
-theta0 = [1.9696      9.0521     0.24324     0.83667]; %4300th
-theta0 = [1.9626      9.0638     0.24049     0.85051]; %4500th
-theta0 = [1.959      9.1515      0.2433      0.86486]; %6500th
-theta0 = [1.9582      9.1592     0.24799     0.87206]; %7000th
+
+theta0= rand(1,4).^2;
+%theta0 = [1.9578      8.3635     0.25871     0.74678]; %1600th
+%theta0 = [1.9075      8.6019     0.39438     0.92227]; %1750th
 
 energyhistory = zeros(1, num_iter) ;
 
@@ -63,13 +55,14 @@ energyhistory = zeros(1, num_iter) ;
 thetahistory = zeros(1+num_iter,length(theta0));
 thetahistory(1,:) = theta0;
 
+theta0 = theta
 
 for iter = 1:num_iter
     rnsource = randn([2, Ntry, num_timepts]);
 
     display(['initiating the ', num2str(iter), 'th iterations...']);
     tic,
-    [derivative, energy] = and_CFD_datagen_mass_derivStat_all_parameters_totVar...
+    [derivative, energy] = and_CFD_datagen_mass_derivStat_all_parameters_totVar2...
                         (initx, tend, theta0, sigV, sigW, num_timepts, rnsource, snapshots, timesample, Ntry);
     display([num2str(iter), 'th iteration : deriv= ' , num2str(derivative')]);
     
@@ -88,5 +81,4 @@ for iter = 1:num_iter
         thetahistory(iter+1,:) = theta0;
 end 
 
-energyhistory_cum = [energyhistory_cum, energyhistory];
 
